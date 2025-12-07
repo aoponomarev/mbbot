8eba1ead93e0cbd7c77201fbbb7a1708422f001e
ao <ponomarev.ux@gmail.com>
2025-12-07 18:25:46 +0300

    Restore Vue init into external scripts (config.js/app.js)

---
+++ config.js
@@ -0,0 +1,8 @@
+// Глобальный конфиг приложения (обычные скрипты, без module/fetch)
+window.appConfig = {
+  defaults: {
+    theme: 'light',
+    perplexityModel: 'sonar-pro',
+    defaultApiKey: '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'
+  }
+};
