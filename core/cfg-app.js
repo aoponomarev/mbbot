88af77ba8b87453f3d1d000f0d66a31f9dffaa1b
ao <ponomarev.ux@gmail.com>
2025-12-10 11:47:46 +0300

    Add automatic container marking system with avto- classes

--- core/cfg-app.js
+++ core/cfg-app.js
@@ -13,5 +13,5 @@ window.appConfig = {
     { value: 'llama-3.1-sonar-small-128k-online', label: 'llama-3.1-sonar-small-128k-online' },
     { value: 'llama-3.1-sonar-large-128k-online', label: 'llama-3.1-sonar-large-128k-online' }
   ],
-  lastCommitMessage: 'Add inverse theme for header dropdowns/tooltips and remove header side padding'
+  lastCommitMessage: 'Add automatic container marking system with avto- classes'
 };
