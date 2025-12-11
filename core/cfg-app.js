f0faaf18307ed0e4bb6778ff0445e00683d0aa78
ao <ponomarev.ux@gmail.com>
2025-12-11 14:39:15 +0300

    Change selected rows highlight color to greenish-cyan

--- core/cfg-app.js
+++ core/cfg-app.js
@@ -13,5 +13,5 @@ window.appConfig = {
     { value: 'llama-3.1-sonar-small-128k-online', label: 'llama-3.1-sonar-small-128k-online' },
     { value: 'llama-3.1-sonar-large-128k-online', label: 'llama-3.1-sonar-large-128k-online' }
   ],
-  lastCommitMessage: 'Remove manual row movement, add Open command for Bybit'
+  lastCommitMessage: 'Change selected rows highlight color to greenish-cyan'
 };
