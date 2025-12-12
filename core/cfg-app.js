db226c7c840a3ef259abca6acfb0ef9282fe6dae
ao <ponomarev.ux@gmail.com>
2025-12-12 21:21:09 +0300

    Add rule for understanding 'внеси в правила' command

--- core/cfg-app.js
+++ core/cfg-app.js
@@ -13,5 +13,5 @@ window.appConfig = {
     { value: 'llama-3.1-sonar-small-128k-online', label: 'llama-3.1-sonar-small-128k-online' },
     { value: 'llama-3.1-sonar-large-128k-online', label: 'llama-3.1-sonar-large-128k-online' }
   ],
-  lastCommitMessage: 'Add cell-coin component and favorites system for coins'
+  lastCommitMessage: 'Add rule for understanding \'внеси в правила\' command'
 };
