f16b9f75b44b43cbb02d3cb137df2443d26bbfcc
ao <ponomarev.ux@gmail.com>
2025-12-14 17:04:22 +0300

    Refactor documentation structure and add mandatory documentation reading principle

--- core/cfg-app.js
+++ core/cfg-app.js
@@ -13,5 +13,5 @@ window.appConfig = {
     { value: 'llama-3.1-sonar-small-128k-online', label: 'llama-3.1-sonar-small-128k-online' },
     { value: 'llama-3.1-sonar-large-128k-online', label: 'llama-3.1-sonar-large-128k-online' }
   ],
-  lastCommitMessage: 'Fix header checkbox deselection and optimize header-coins component'
+  lastCommitMessage: 'Refactor documentation structure and add mandatory documentation reading principle'
 };
