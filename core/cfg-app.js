1d99d706c4871583d997e57e9b6ab02668b5d40b
ao <ponomarev.ux@gmail.com>
2025-12-15 15:42:36 +0300

    Mobile: adaptive spacing and coin column styles

--- core/cfg-app.js
+++ core/cfg-app.js
@@ -13,5 +13,5 @@ window.appConfig = {
     { value: 'llama-3.1-sonar-small-128k-online', label: 'llama-3.1-sonar-small-128k-online' },
     { value: 'llama-3.1-sonar-large-128k-online', label: 'llama-3.1-sonar-large-128k-online' }
   ],
-  lastCommitMessage: 'Review: fix table headers order'
+  lastCommitMessage: 'Mobile: adaptive spacing and coin column styles'
 };
