f14d61dd97b0a5e924053635e8a87006b833cf96
ao <ponomarev.ux@gmail.com>
2025-12-15 08:58:39 +0300

    Start of day: 12-15

--- core/cfg-app.js
+++ core/cfg-app.js
@@ -13,5 +13,5 @@ window.appConfig = {
     { value: 'llama-3.1-sonar-small-128k-online', label: 'llama-3.1-sonar-small-128k-online' },
     { value: 'llama-3.1-sonar-large-128k-online', label: 'llama-3.1-sonar-large-128k-online' }
   ],
-  lastCommitMessage: 'Add docs/ to gitignore and remove from index'
+  lastCommitMessage: 'Start of day: 12-15'
 };
