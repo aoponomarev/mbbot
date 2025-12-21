398c15ceaaf0d368069f91d02df3e05f841a0e26
ao <ponomarev.ux@gmail.com>
2025-12-21 09:45:13 +0300

    Fix file ranking number column color

--- core/cfg-app.js
+++ core/cfg-app.js
@@ -13,5 +13,5 @@ window.appConfig = {
     { value: 'llama-3.1-sonar-small-128k-online', label: 'llama-3.1-sonar-small-128k-online' },
     { value: 'llama-3.1-sonar-large-128k-online', label: 'llama-3.1-sonar-large-128k-online' }
   ],
-  lastCommitMessage: 'Fix cross-references in documentation'
+  lastCommitMessage: 'Fix file ranking number column color'
 };
