3f45b6c2c262609eb45af573bac726cd7a0cee4d
ao <ponomarev.ux@gmail.com>
2025-12-21 14:03:47 +0300

    Refactor guide-ii.md structure and update mass edits protocol

--- core/cfg-app.js
+++ core/cfg-app.js
@@ -13,5 +13,5 @@ window.appConfig = {
     { value: 'llama-3.1-sonar-small-128k-online', label: 'llama-3.1-sonar-small-128k-online' },
     { value: 'llama-3.1-sonar-large-128k-online', label: 'llama-3.1-sonar-large-128k-online' }
   ],
-  lastCommitMessage: 'Fix file ranking number column color'
+  lastCommitMessage: 'Refactor guide-ii.md structure and update mass edits protocol'
 };
